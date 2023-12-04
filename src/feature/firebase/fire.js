d7dd476d5da44851fc0ebb0be8700406bbba7e9e
formbdance <domero100@gmail.com>
2023-12-04 15:33:42 +0700

    update

--- src/feature/firebase/fire.js
+++ src/feature/firebase/fire.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJt5-_jJQq0aNj4UZxVFATw1ZT-7KI85c",
-  authDomain: "reactauth-4e52f.firebaseapp.com",
-  projectId: "reactauth-4e52f",
-  storageBucket: "reactauth-4e52f.appspot.com",
-  messagingSenderId: "689601901707",
-  appId: "1:689601901707:web:c7358b46aa2ca175fad08e",
-  measurementId: "G-L8H5X2CK2F"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
